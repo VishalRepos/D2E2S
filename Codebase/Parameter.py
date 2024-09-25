b22edf1dcb8745129549d9e0a8e768df33942082
VishalRepos <127038262+VishalRepos@users.noreply.github.com>
2024-09-25 18:55:59 +0530

    Updating Parameter file for Deberta

--- Codebase/Parameter.py
+++ Codebase/Parameter.py
@@ -34,18 +34,18 @@ def train_argparser():
     parser.add_argument('--is_bidirect', default=True, help='Do use bi-RNN layer.')
     parser.add_argument('--use_gated', default=False, help='Do use gcnconv and gatedgraphconv.')
     parser.add_argument('--hidden_dim', type=int, default=384, help='hidden layer dimension.')
-    parser.add_argument('--emb_dim', type=int, default=768, help='Word embedding dimension.')
+    parser.add_argument('--emb_dim', type=int, default=1024, help='Word embedding dimension.')  # Updated for DeBERTa v3
     parser.add_argument('--lstm_layers', type=int, default=2, help='Number of LSTM layers.')
     parser.add_argument('--lstm_dim', type=int, default=384, help='dimension of lstm cell')
     parser.add_argument('--prefix', type=str, default="data/", help='dataset and embedding path prefix')
     parser.add_argument('--span_generator', type=str, default="Max", choices=["Max", "Average"], help='option: Max, Average')
     parser.add_argument('--attention_heads', default=1, type=int, help='number of multi-attention heads')
     parser.add_argument('--num_layers', type=int, default=2, help='Num of GCN layers.')
-    parser.add_argument('--mem_dim', type=int, default=768, help='mutual biaffine men dim.')
+    parser.add_argument('--mem_dim', type=int, default=1024, help='mutual biaffine men dim.')  # Updated for DeBERTa v3
     parser.add_argument('--gcn_dropout', type=float, default=0.2, help='GCN layer dropout rate.')
     parser.add_argument('--pooling', default='avg', type=str, help='[max, avg, sum]')
     parser.add_argument('--gcn_dim', type=int, default=300, help='dimension of gcn')
-    parser.add_argument('--bert_feature_dim', type=int, default=768, help='dimension of pretrained bert feature')
+    parser.add_argument('--bert_feature_dim', type=int, default=1024, help='dimension of pretrained bert feature')  # Updated for DeBERTa v3
     parser.add_argument("--seed", default=42, type=int, help="Random seed for initialization")
 
     parser.add_argument('--max_span_size', type=int, default=8, help="Maximum size of spans")
@@ -56,19 +56,20 @@ def train_argparser():
     parser.add_argument('--neg_entity_count', type=int, default=100,help="The number of negative entities samples for each sample")
     parser.add_argument('--neg_triple_count', type=int, default=100, help="The number of negative triplets samples for each sample")
 
-    parser.add_argument('--tokenizer_path', default='bert-base-uncased', type=str, help="tokenizer load_path")
+    # Update the tokenizer path and model name for DeBERTa v3
+    parser.add_argument('--tokenizer_path', default='microsoft/deberta-v3-base', type=str, help="tokenizer load_path")
     parser.add_argument('--cpu', action='store_true', default=False,help="If true, train/evaluate on CPU even if a CUDA device is available")
     parser.add_argument('--size_embedding', type=int, default=25, help="Dimensionality of size embedding,E_{k}")
     parser.add_argument('--sampling_processes', type=int, default=4,help="The number of sampling processes")
     parser.add_argument('--prop_drop', type=float, default=0.1, help="Probability of dropout used in D2E2S")
-    parser.add_argument('--freeze_transformer', action='store_true', default=False, help="Freezing Bert's parameters for easy test")
+    parser.add_argument('--freeze_transformer', action='store_true', default=False, help="Freezing DeBERTa's parameters for easy test")
     parser.add_argument('--batch_size', type=int, default=16, help="Training batch size")
     parser.add_argument('--epochs', type=int, default=120, help="Number of epochs")
     parser.add_argument('--lr', type=float, default=5e-5, help="Learning rate")
     parser.add_argument('--lr_warmup', type=float, default=0.1, help="Proportion of total train iterations to warmup in linear increase/decrease schedule")
     parser.add_argument('--log_path', type=str,default="log/", help="Path do directory where training/evaluation logs are stored")
     parser.add_argument('--train_log_iter', type=int, default=1, help="Log training process every x iterations")
-    parser.add_argument('--pretrained_bert_name', default='bert-base-uncased', type=str)
+    parser.add_argument('--pretrained_bert_name', default='microsoft/deberta-v3-base', type=str)  # Updated for DeBERTa v3
     parser.add_argument('--weight_decay', type=float, default=0.01, help="Weight decay to apply")
     parser.add_argument('--max_grad_norm', type=float, default=1.0, help="Maximum gradient norm")
     parser.add_argument('--init_eval', action='store_true', default=False, help="If true, evaluate validation set before training")
